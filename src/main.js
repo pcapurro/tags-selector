35431c13cdcfd8f62563cc562062dc3592a53705
pcapurro <fossa634@gmail.com>
2026-07-21 22:06:47 +0200

    added ontology interaction tools

--- src/main.js
+++ src/main.js
@@ -1,9 +1,37 @@
-async function main()
+// URL du fichier .rdf de l'ontologie
+const   rdfUrl = "https://Fondation-SCP.github.io/tags-selector/resources/ontology/structure-tags.rdf"
+
+// Instanciation de Comunica pour faire des requêtes SPARQL au fichier .rdf
+const   engine = new Comunica.QueryEngine();
+
+// Renvoie tous les tags racine
+async function getRoots()
 {
-    const   rdfUrl = "https://Fondation-SCP.github.io/tags-selector/resources/ontology/structure-tags.rdf"
+    const   query = `
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
-    const   engine = new Comunica.QueryEngine();
+        SELECT ?classe
+        WHERE {
+            ?classe a owl:Class .
 
+            FILTER NOT EXISTS {
+                ?classe rdfs:subClassOf ?parent .
+            }
+        }
+        `;
+
+    const   result = await engine.queryBindings(
+        query,
+        { sources: [rdfUrl] }
+    );
+
+    return await result.toArray();
+}
+
+// Renvoie tous les tags
+async function getAll()
+{
     const   query = `
         PREFIX owl: <http://www.w3.org/2002/07/owl#>
 
@@ -18,17 +46,26 @@ async function main()
         { sources: [rdfUrl] }
     );
 
-    result.on("data", (binding) => {
-        console.log(binding.get("classe").value);
-    });
+    return await result.toArray();
+}
 
-    result.on("end", () => {
-        console.log("Fin");
-    });
+// Affiche une liste de tags passée en paramètre
+function printList(tags)
+{
+    for (const element of tags)
+    {
+        const   uri = element.get("classe").value;
+        const   name = uri.split("#").pop();
 
-    result.on("error", (error) => {
-        console.error(error);
-    });
+        console.log("> '" + name + "'");
+    }
+
+    console.log("Total: " + tags.length);
+}
+
+async function main()
+{
+    printList(await getRoots());
 }
 
 main();
